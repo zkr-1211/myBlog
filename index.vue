cf72908860f8cf06b75460ff1d620c9a9c6110f4
zkr-1211 <81360055+zkr-1211@users.noreply.github.com>
2021-11-14 18:18:16 +0800

    test1

---
+++ index.vue
@@ -0,0 +1,17 @@
+<!--  -->
+<template>
+  <div class="body">123</div>
+</template>
+
+<script>
+  import { onMounted, defineComponent } from 'vue'
+  export default defineComponent({
+    components: {},
+    name: '',
+    setup() {
+      onMounted(() => {})
+      return {}
+    }
+  })
+</script>
+<style lang="scss" scoped></style>
